07436c2247ded0db13f0d9ceba14cb2e011bfdfb
Xico Trujano <xtrujano@mac.com>
2025-03-03 12:56:58 -0600

    Weather App

--- lab/mosaico/js/weather-api.js
+++ lab/mosaico/js/weather-api.js
@@ -1,7 +1,7 @@
 
         const weatherImage = document.getElementById('icon-weather');
         async function getWeather() {
-            const apiKey = '5f4204b5e1c30da45f9a0a2b2679138e';
+            const apiKey = 'KEY';
             const city = 'Mexico City';
             const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`);
             const data = await response.json();
